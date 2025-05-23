b85eef0eab4a4fb2f306ecb7a81e6260f3b0f33c
Jatin Rao <jatinrao@Jatins-Mac-mini.local>
2025-05-23 18:13:38 +0530

    deployment debug

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import { withIntlayer } from "next-intlayer/server";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+output: 'standalone',
 };
 
 export default withIntlayer(nextConfig);
